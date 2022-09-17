180cfdf58dd24fe6a60bf68037ea30d05cc21f9d
zjl9959 <1522480382@qq.com>
2022-09-17 14:11:03 +0000

    Deploying to gh-pages from @ zjl9959/algviz.com@f23c8e3f6dcf5355993ba0705d8ada8aea92d80e 🚀

--- assets/js/jquery.toc.en.js
+++ assets/js/jquery.toc.en.js
@@ -5,7 +5,7 @@
     var defaults = {
       noBackToTopLinks: false,
       minimumHeaders: 2,
-      title: '目录：',
+      title: 'TOC:',
       headers: 'h1, h2, h3, h4, h5, h6',
       listType: 'ul', // values: [ol|ul]
       showEffect: 'show', // values: [show|slideDown|fadeIn|none]
